230abeeed14890596e6dbf3b8d5a82708b0479c5
Kartavchenko <stranger1301078@gmail.com>
2022-05-01 14:03:51 +0200

    murkup-hw-8-6

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -5,6 +5,9 @@
   menuBtnRef.addEventListener("click", () => {
     const expanded =
       menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+    
+    document.body.classList.toggle("is-open");
+    
     menuBtnRef.classList.toggle("is-active");
     menuBtnRef.setAttribute("aria-expanded", !expanded);
     mobileMenuRef.classList.toggle("is-open");
